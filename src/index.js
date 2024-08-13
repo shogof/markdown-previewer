26fc3a821d45812c3eea76e72befd68d9ac4a6ae
Shegofa <shogofadeveloper12@gmail.com>
2024-08-13 23:42:22 +0400

    files added

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MarkdownPreviewer from './MarkdownPreviewer';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <MarkdownPreviewer />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
